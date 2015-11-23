bf71225e0d2d38b8af98708a645710c39a66e3b5
Justin Frazer <jfrazer@mines.edu>
2015-11-23 14:29:51 -0700

    edited home page

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,12 +3,14 @@
 <head>
     <title>Group Project</title>
     <style>
-
+        .title {
+            font-size: 400%;
+        }
     </style>
 </head>
 <body>
 <div class="title">
-
+    CSCONNECT<hr>
 </div>
 </body>
 </html>
