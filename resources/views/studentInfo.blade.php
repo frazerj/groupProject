2735a80e72a7f41d8b5d60d21a23aec31f5ff31e
Justin Frazer <jfrazer@mines.edu>
2015-11-23 15:40:04 -0700

    edited info page

---
+++ resources/views/studentInfo.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Student Info</title>
+    <style>
+        .title {
+            font-size: 400%;
+        }
+    </style>
+</head>
+<body>
+<div class="title">
+    Student Info
+    <hr>
+</div>
+</body>
+</html>
