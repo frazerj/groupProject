d806cb92d61bcde369b60c396904709b15dec628
kdaytacos <kday@Mymail.mines.edu>
2015-11-24 08:18:49 -0700

    Added .css files for the homepage and student info, finished the pages

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,17 +1,19 @@
 <!DOCTYPE html>
 <html>
-<head>
-    <title>Group Project</title>
-    <style>
-        .title {
-            font-size: 400%;
-        }
-    </style>
-</head>
-<body>
-<div class="title">
-    CSCONNECT
-    <hr>
-</div>
-</body>
+<link rel="stylesheet" type="text/css" href="home.blade.css">
+	<head>
+		<title>Login Page</title>
+			<div id="backBorder">
+				<h1 id="title"> CSCONNECT </h1>
+				<div id="menu">
+					Log In<br>
+					<div id="input">
+						Email: <input type="text" name="email" style="width: 203px;"><br><br>
+						Password: <input type="text" name="password" style="width: 180px;">
+						<button type="button">Login</button><br>
+						<p style="font-size:11px" align="center">New user? Click <a href="INSERT URL HERE">here</a> to create an account!</p>
+					</div>
+				</div>
+			</div>
+	</head>
 </html>
--- resources/views/studentInfo.blade.php
+++ resources/views/studentInfo.blade.php
@@ -1,17 +1,25 @@
 <!DOCTYPE html>
 <html>
-<head>
-    <title>Student Info</title>
-    <style>
-        .title {
-            font-size: 400%;
-        }
-    </style>
-</head>
-<body>
-<div class="title">
-    Student Info
-    <hr>
-</div>
-</body>
+<link rel="stylesheet" type="text/css" href="studentInfo.css">
+	<head>
+		<title>Student Info</title>
+			<div id="backBorder">
+				<h1 id="titleHeader"> CSCONNECT </h1>
+				<div id="menu">
+					Student Info<br>
+					<div id="studentInfo">
+						Name: <input type="text" name="name" style="width: 180px;"><br>
+						Preferred Languages: <input type="text" name="prefLang" style="width: 180px;"><br>
+						Team Style:
+						<select name="teamStyle" style="width: 185px;">
+						  <option value="social">Social</option>
+						  <option value="competitive">Competitive</option>
+						  <option value="neither">Don't Care</option>
+						</select><br>
+						Courses Taken: <input type="text" name="coursesTaken" style="width: 180px;"><br>
+						<p align="left"><button type="button" align="left">Edit Information</button></p>
+					</div>
+				</div>
+			</div>
+	</head>
 </html>
